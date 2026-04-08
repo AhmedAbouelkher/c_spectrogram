d8d6d6a2dd8f020190fd9705d72b3cde771bce6f
Ahmed Abouelkheir <ahmedabouelkher1@gmail.com>
2026-04-08 20:47:28 +0200

    Enhance image processing functionality and update build configuration. Added grayscale support for image generation, improved file path handling, and renamed the output binary to 'spectrogram'. Introduced a README file for project documentation.

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@ CFLAGS = -I/opt/homebrew/include $$(/opt/homebrew/bin/pkg-config --cflags raylib
 LDFLAGS = -L/opt/homebrew/lib -lsndfile $$(/opt/homebrew/bin/pkg-config --libs raylib)
 
 build: clean
-	$(CC) $(CFLAGS) -O3 -o stft main.c $(LDFLAGS)
+	$(CC) $(CFLAGS) -O3 -o spectrogram main.c $(LDFLAGS)
 
 debug: clean
-	$(CC) $(CFLAGS) -g -O0 -o stft main.c $(LDFLAGS)
+	$(CC) $(CFLAGS) -g -O0 -o spectrogram main.c $(LDFLAGS)
 
 clean:
-	rm -f stft
+	rm -f spectrogram
--- main.c
+++ main.c
@@ -8,10 +8,20 @@
 #include "raylib.h"
 #include "sndfile.h"
 
+#ifdef _WIN32
+#define PATH_JOIN_SEPARATOR "\\"
+#else
+#define PATH_JOIN_SEPARATOR "/"
+#endif
+
 #define ARRAY_LEN(x) ((int)(sizeof(x) / sizeof(x[0])))
 
+#define MAX_FILEPATH_LENGTH 1 << 12
+#define RAYLIB_WINDOW_ASPECT_RATION 1200.0 / 600.0
+
 size_t WINDOW_SIZE = 1 << 10;
-size_t STEP_SIZE = 1 << 10 / 2;
+
+bool global_isGrayscale = false;
 
 void hammingWindow(float in[], size_t size) {
   // Apply a tapering window to reduce spectral leakage before FFT.
@@ -68,7 +78,8 @@ static void heatColor(float t, unsigned char rgb[3]) {
   rgb[2] = (unsigned char)(255.0f * b);
 }
 
-int saveImage(const char *filename, float *data, int width, int height) {
+int saveImage(const char *filename, float *data, int width, int height,
+              bool isGrayscale) {
   FILE *f = fopen(filename, "wb");
   if (!f)
     return -1;
@@ -90,7 +101,13 @@ int saveImage(const char *filename, float *data, int width, int height) {
       float val = data[y * width + x];
       float t = (val - min_val) / range;
       unsigned char rgb[3];
-      heatColor(t, rgb);
+      if (isGrayscale) {
+        rgb[0] = (unsigned char)(t * 255.0f);
+        rgb[1] = (unsigned char)(t * 255.0f);
+        rgb[2] = (unsigned char)(t * 255.0f);
+      } else {
+        heatColor(t, rgb);
+      }
       fwrite(rgb, 1, 3, f);
     }
   }
@@ -98,7 +115,8 @@ int saveImage(const char *filename, float *data, int width, int height) {
   return 0;
 }
 
-int createRayImage(Image *dst, float *data, int width, int height) {
+int createRayImage(Image *dst, float *data, int width, int height,
+                   bool isGrayscale) {
   float min_val = 1000.0f, max_val = -1000.0f;
   // Find global min/max for normalization to the heatmap range [0, 1].
   for (int i = 0; i < width * height; i++) {
@@ -110,17 +128,20 @@ int createRayImage(Image *dst, float *data, int width, int height) {
   float range = max_val - min_val;
   if (range < 1e-6f)
     range = 1.0f;
-
-  // ImageFlipHorizontal(dst);
-
   for (int y = 0; y < height; y++) {
-    int srcY = height - 1 - y;
+    int srcY = height - 1 - y; // to flip the image vertically
     for (int x = 0; x < width; x++) {
       float val = data[srcY * width + x];
       float t = (val - min_val) / range;
-      unsigned char rgb[3];
-      heatColor(t, rgb);
-      Color color = {rgb[0], rgb[1], rgb[2], 255};
+      Color color = {0};
+      if (isGrayscale) {
+        unsigned char gray = (unsigned char)(t * 255.0f);
+        color.r = gray, color.g = gray, color.b = gray, color.a = 255;
+      } else {
+        unsigned char rgb[3];
+        heatColor(t, rgb);
+        color.r = rgb[0], color.g = rgb[1], color.b = rgb[2], color.a = 255;
+      }
       ImageDrawPixel(dst, x, y, color);
     }
   }
@@ -128,92 +149,10 @@ int createRayImage(Image *dst, float *data, int width, int height) {
   return 0;
 }
 
-float *processAudio(char *srcPath, int *dstImageWidth, int *dstImageHeight);
-
-int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    printf("You should provide the audio file path\n");
-    return -1;
-  }
-
-  // MARK:- Audio
-
-  int imageWidth = 0, imageHeight = 0;
-
-  float *imageData = processAudio(argv[1], &imageWidth, &imageHeight);
-
-  if (imageData == NULL || imageWidth == 0 || imageHeight == 0) {
-    printf("Failed to process the audio file\n");
-    printf("is image null %d\n", imageData == NULL);
-    printf("is imageWidth = 0 %d\n", imageWidth == 0);
-    printf("is imageHeight = 0 %d\n", imageHeight == 0);
-    return -2;
-  }
-
-  if (argc > 2) {
-    char *outputFilePath = argv[2];
-    printf("Saving image...\n");
-    if (saveImage(outputFilePath, imageData, imageWidth, imageHeight) < 0) {
-      printf("FAILED TO SAVE THE IMAGE FILE\n");
-      return -1;
-    }
-    printf("Image saved: %s\n", outputFilePath);
-
-    free(imageData);
-
-    return 0;
-  }
-
-  // MARK:- Raylib
-
-  const float windowAspectRatio = 1000.0 / 600.0;
-  size_t windowH = 600;
-  size_t windowW = windowH * windowAspectRatio;
-
-  InitWindow(windowW, windowH, "STFT");
-  SetTargetFPS(60);
-
-  Image img = GenImageColor(imageWidth, imageHeight, WHITE);
-  if (createRayImage(&img, imageData, imageWidth, imageHeight) < 0) {
-    printf("FAILED TO Create THE IMAGE for raylib\n");
-    return -1;
-  }
-  Texture2D texture = LoadTextureFromImage(img);
-  if (!IsTextureValid(texture)) {
-    printf("FAILED TO load texture\n");
-    return -1;
-  }
-
-  free(imageData);
-
-  while (!WindowShouldClose()) {
-    BeginDrawing();
-    ClearBackground(BLACK);
-
-    // Build Spectrogram Image Texture
-    Rectangle src = {0, 0, texture.width, texture.height};
-    float scaledWidth = (float)texture.height * windowAspectRatio;
-    if (scaledWidth < windowW)
-      scaledWidth = windowW;
-    Rectangle dest = {
-        (GetScreenWidth() - scaledWidth) * 0.5f,
-        (GetScreenHeight() - texture.height) * 0.5f,
-        scaledWidth,
-        texture.height,
-    };
-    Vector2 origin = {0.0f, 0.0f};
-    DrawTexturePro(texture, src, dest, origin, 1, WHITE);
-
-    EndDrawing();
-  }
-
-  UnloadTexture(texture);
-  CloseWindow();
-
-  return 0;
-}
-
+// result manual deallocation is required
 float *processAudio(char *srcPath, int *dstImageWidth, int *dstImageHeight) {
+  size_t STEP_SIZE = (WINDOW_SIZE) / 2;
+
   SF_INFO fileInfo;
   memset(&fileInfo, 0, sizeof(fileInfo));
 
@@ -305,4 +244,199 @@ float *processAudio(char *srcPath, int *dstImageWidth, int *dstImageHeight) {
   free(channelBuffer);
 
   return imageData;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("You should provide the audio file path\n");
+    return -1;
+  }
+
+  // MARK:- Audio
+  int imageWidth = 0, imageHeight = 0;
+
+  float *imageData = processAudio(argv[1], &imageWidth, &imageHeight);
+
+  if (imageData == NULL || imageWidth == 0 || imageHeight == 0) {
+    printf("Failed to process the audio file\n");
+    printf("is image null %d\n", imageData == NULL);
+    printf("is imageWidth = 0 %d\n", imageWidth == 0);
+    printf("is imageHeight = 0 %d\n", imageHeight == 0);
+    return -2;
+  }
+
+  // MARK:- Raylib
+
+  size_t windowH = 600;
+  size_t windowW = windowH * RAYLIB_WINDOW_ASPECT_RATION;
+
+  InitWindow(windowW, windowH, "STFT");
+  SetTargetFPS(60);
+
+  Image img = GenImageColor(imageWidth, imageHeight, WHITE);
+
+  if (createRayImage(&img, imageData, imageWidth, imageHeight,
+                     global_isGrayscale) < 0) {
+    printf("FAILED TO Create THE IMAGE for raylib\n");
+    return -1;
+  }
+
+  Texture2D texture = LoadTextureFromImage(img);
+  if (!IsTextureValid(texture)) {
+    printf("FAILED TO load texture\n");
+    return -1;
+  }
+
+  float fontSize = 12;
+
+  const char *msgText = NULL;
+  double msgTime = 0;
+
+  Rectangle saveBtnRect = {0, 10, 125, 35};
+  const char *saveBtnText = "Save as Raw Image";
+
+  Rectangle gsToggleBtnRect = {10, 10, 80, 30};
+
+  while (!WindowShouldClose()) {
+    // START: Save button
+    saveBtnRect.x = GetScreenWidth() - saveBtnRect.width - 10;
+    Vector2 mouse = GetMousePosition();
+    if (1) {
+      bool btnHovering = CheckCollisionPointRec(mouse, saveBtnRect);
+      bool btnClicked = btnHovering && IsMouseButtonPressed(MOUSE_LEFT_BUTTON);
+
+      if (btnHovering) {
+        SetMouseCursor(MOUSE_CURSOR_POINTING_HAND);
+      } else {
+        SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+      }
+      if (btnClicked) {
+        const char *fileNoExt = GetFileNameWithoutExt(argv[1]);
+        char outputDir[MAX_FILEPATH_LENGTH];
+        strcpy(outputDir, TextFormat(".%soutput", PATH_JOIN_SEPARATOR));
+        if (MakeDirectory(outputDir) != 0) {
+          printf("Failed to create output directory. Using the current "
+                 "directory...\n");
+          strcpy(outputDir, GetWorkingDirectory());
+        }
+        char *imgColorType = "_colored";
+        if (global_isGrayscale) {
+          imgColorType = "_grayscale";
+        }
+        const char *fp =
+            TextFormat("%s%s%s%s%s", outputDir, PATH_JOIN_SEPARATOR, fileNoExt,
+                       imgColorType, ".ppm");
+        printf("Saving image to: %s...\n", fp);
+        if (saveImage(fp, imageData, imageWidth, imageHeight,
+                      global_isGrayscale) < 0) {
+          printf("FAILED TO SAVE THE IMAGE FILE: %s\n", fp);
+          continue;
+        }
+        printf("Image saved: %s\n", fp);
+        // "Image saved at "
+        msgText = TextFormat("Image saved at: %s", fp);
+        msgTime = GetTime();
+      }
+    }
+    // END: save button
+
+    // START: Grayscale toggle button
+    if (1) {
+      gsToggleBtnRect.x = saveBtnRect.x - gsToggleBtnRect.width - 10;
+      bool btnHovering = CheckCollisionPointRec(mouse, gsToggleBtnRect);
+      bool btnClicked = btnHovering && IsMouseButtonPressed(MOUSE_LEFT_BUTTON);
+      if (btnHovering) {
+        SetMouseCursor(MOUSE_CURSOR_POINTING_HAND);
+      } else {
+        SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+      }
+      if (btnClicked) {
+        global_isGrayscale = !global_isGrayscale;
+        if (createRayImage(&img, imageData, imageWidth, imageHeight,
+                           global_isGrayscale) < 0) {
+          printf("FAILED TO Create THE IMAGE for raylib\n");
+          continue;
+        }
+
+        texture = LoadTextureFromImage(img);
+        if (!IsTextureValid(texture)) {
+          printf("FAILED TO load texture\n");
+          continue;
+        }
+      }
+    }
+    // END: Grayscale toggle button
+
+    // Clear the msgText after 5 seconds
+    if (msgText != NULL && (GetTime() - msgTime) > 5.0) {
+      msgText = NULL;
+    }
+
+    BeginDrawing();
+    ClearBackground(BLACK);
+
+    // START: Draw Spectrogram Image Texture
+    if (1) {
+      Rectangle src = {0, 0, texture.width, texture.height};
+      float scaledWidth = (float)texture.height * RAYLIB_WINDOW_ASPECT_RATION;
+      if (scaledWidth < windowW)
+        scaledWidth = windowW;
+      Rectangle dest = {
+          (GetScreenWidth() - scaledWidth) * 0.5f,
+          (GetScreenHeight() - texture.height) * 0.5f,
+          scaledWidth,
+          texture.height,
+      };
+      Vector2 origin = {0.0f, 0.0f};
+      DrawTexturePro(texture, src, dest, origin, 0, WHITE);
+    }
+    // END: Draw Spectrogram Image Texture
+
+    // START: Draw Save Button
+    if (1) {
+      DrawRectangleRounded(saveBtnRect, 0.3f, 12, DARKPURPLE);
+      float saveBtnTextWidth = MeasureText(saveBtnText, fontSize);
+      DrawText(
+          saveBtnText,
+          (int)(saveBtnRect.x + saveBtnRect.width / 2 - saveBtnTextWidth / 2),
+          (int)(saveBtnRect.y + saveBtnRect.height / 2 - fontSize / 2),
+          fontSize, WHITE);
+
+      // END: Draw Save Button
+
+      // START: Draw Grayscale Toggle Button
+      char *gsToggleBtnText = "Grayscale";
+      Color gsToggleBgColor = GRAY;
+      Color gsToggleTxtColor = WHITE;
+      if (global_isGrayscale) {
+        gsToggleBtnText = "Colored";
+        gsToggleBgColor = ORANGE;
+        gsToggleTxtColor = BLACK;
+      }
+      DrawRectangleRec(gsToggleBtnRect, gsToggleBgColor);
+      float gsTextWidth = MeasureText(gsToggleBtnText, fontSize);
+      DrawText(
+          gsToggleBtnText,
+          (int)(gsToggleBtnRect.x + gsToggleBtnRect.width / 2 -
+                gsTextWidth / 2),
+          (int)(gsToggleBtnRect.y + gsToggleBtnRect.height / 2 - fontSize / 2),
+          fontSize, gsToggleTxtColor);
+    }
+    // END: Draw Grayscale Toggle Button
+
+    if (msgText != NULL) {
+      int msgTextFontSize = 17;
+      float msgTextWidth = MeasureText(msgText, msgTextFontSize);
+      size_t xPos = (GetScreenWidth() - msgTextWidth) / 2;
+      DrawText(msgText, xPos, 10, msgTextFontSize, RED);
+    }
+
+    EndDrawing();
+  }
+
+  UnloadTexture(texture);
+  free(imageData);
+  CloseWindow();
+
+  return 0;
 }
